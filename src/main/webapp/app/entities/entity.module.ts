a5073b1a4f15dcd0b0e5e199ec34accb4bb2f0d6
buminda <Codegen321>
2020-02-09 21:02:33 +1000

    Add models

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,18 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'client',
+        loadChildren: () => import('./client/client.module').then(m => m.PmsClientModule)
+      },
+      {
+        path: 'address',
+        loadChildren: () => import('./address/address.module').then(m => m.PmsAddressModule)
+      },
+      {
+        path: 'patient',
+        loadChildren: () => import('./patient/patient.module').then(m => m.PmsPatientModule)
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
